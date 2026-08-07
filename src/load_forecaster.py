29f20b51fa2de1057da0756e352e590ac94cda45
dmontoya-25 <dmontoya@stevens.edu>
2026-08-07 11:11:46 -0400

    Create load_forecaster.py

---
+++ src/load_forecaster.py
@@ -0,0 +1,35 @@
+"""
+Implements simple short-term forecasting: rolling mean and seasonal weighting.
+Module name: load_forecaster.py
+"""
+import pandas as pd
+
+class LoadForecaster:
+    def __init__(self, merged_df: pd.DataFrame):
+        self.df = merged_df.copy()
+        self.df = self.df.sort_values('Timestamp')
+
+    def prepare_features(self):
+        self.df['hour'] = self.df['Timestamp'].dt.hour
+        self.df['weekday'] = self.df['Timestamp'].dt.weekday
+        return self.df
+
+    def forecast_next_hour(self):
+        """
+        Uses last N load values for rolling average forecasting.
+        """
+        recent = self.df['Load_Percent'].tail(6)  # last ~1 hour of 10-minute data
+        return recent.mean()
+
+    def forecast_next_24h(self):
+        """
+        Projects next 24 hours based on mean of each hour of day historically.
+        """
+        self.df['hour'] = self.df['Timestamp'].dt.hour
+        hour_profile = self.df.groupby('hour')['Load_Percent'].mean()
+
+        forecast = []
+        for h in range(24):
+            forecast.append(hour_profile.get(h, hour_profile.mean()))
+
+        return pd.Series(forecast)
